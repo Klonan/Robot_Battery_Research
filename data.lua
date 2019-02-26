ae04da25674bf458ebd55cbf283959c063a29042
Klonan <11986037+Klonan@users.noreply.github.com>
2019-02-26 17:47:29 +0100

    Add thumnail, 0.17 fixes

--- data.lua
+++ data.lua
@@ -18,9 +18,9 @@ data:extend
       count_formula = "100*L",
       ingredients =
       {
-        {"science-pack-1", 1},
-        {"science-pack-2", 1},
-        {"science-pack-3", 1},
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"chemical-science-pack", 1},
       },
       time = 30
     },
@@ -46,9 +46,9 @@ data:extend
       count_formula = "100*L",
       ingredients =
       {
-        {"science-pack-1", 1},
-        {"science-pack-2", 1},
-        {"science-pack-3", 1},
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"chemical-science-pack", 1},
         {"production-science-pack", 1},
       },
       time = 45
@@ -75,11 +75,11 @@ data:extend
       count_formula = "100*L",
       ingredients =
       {
-        {"science-pack-1", 1},
-        {"science-pack-2", 1},
-        {"science-pack-3", 1},
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"chemical-science-pack", 1},
         {"production-science-pack", 1},
-        {"high-tech-science-pack", 1},
+        {"utility-science-pack", 1},
       },
       time = 60
     },
@@ -105,11 +105,11 @@ data:extend
       count_formula = "100*L",
       ingredients =
       {
-        {"science-pack-1", 1},
-        {"science-pack-2", 1},
-        {"science-pack-3", 1},
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"chemical-science-pack", 1},
         {"production-science-pack", 1},
-        {"high-tech-science-pack", 1},
+        {"utility-science-pack", 1},
         {"space-science-pack", 1},
       },
       time = 60
